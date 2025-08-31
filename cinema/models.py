79619c5a15f2d5a98b9dd0355a52629449f79a9c
Lev <lev01111989@gmail.com>
2025-08-31 17:39:27 +0200

    Attempt2

--- cinema/models.py
+++ cinema/models.py
@@ -3,5 +3,5 @@ from django.db import models
 
 class Movie(models.Model):
     title = models.CharField(max_length=100)
-    description = models.CharField(max_length=255, null=True, blank=True)
-    duation = models.IntegerField(null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+    duration = models.PositiveIntegerField(null=True, blank=True)
--- cinema/urls.py
+++ cinema/urls.py
@@ -3,7 +3,8 @@ from django.urls import path
 
 
 app_name = "cinema"
+
 urlpatterns = [
-    path("movies", movie_list, name="movie_list"),
-    path("movies/<int:pk>", movie_detail, name="movie_detail"),
+    path("movies/", movie_list, name="movie_list"),
+    path("movies/<int:pk>/", movie_detail, name="movie_detail"),
 ]
